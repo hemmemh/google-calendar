0b362f49733d9a420e65e51faa951a30606fd826
tagir <makhmutov_tagir@bk.ru>
2025-07-08 11:45:10 +0500

    chore: apdate docker

--- google-calendar-back/src/main.ts
+++ google-calendar-back/src/main.ts
@@ -5,10 +5,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: process.env.FRONT_URL,
     credentials: true,
   });
   app.use(cookieParser()); 
-  await app.listen(5100);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
